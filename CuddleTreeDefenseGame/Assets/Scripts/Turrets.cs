aeac31e1c29b7a1bde23f00275d52d675ccde2c2
Sebastian Lundgren <relinkz@users.noreply.github.com>
2021-03-26 18:54:08 +0100

    Fix BasicTurret aim at a monster (#6)
    Script that make the turret rotate to the first EnemyDragon it can find on startup.
    If the target distance is 10  or above it rotates in circles (searching for enemies)

--- CuddleTreeDefenseGame/Assets/Scripts/Turrets.cs
+++ CuddleTreeDefenseGame/Assets/Scripts/Turrets.cs
@@ -1,12 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Turrets : MonoBehaviour
 {
-    // Update is called once per frame
+    private EnemyDragon target;
+    [SerializeField] public float RotationSpeed = 20;
+
+    private void OnEnable()
+    {
+        target = FindObjectOfType<EnemyDragon>();
+    }
+
+    void rotateToTarget()
+    {
+        Vector3 vectorToTarget = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        angle -= 90; // we need to apply a offset since we want the tip of the cannon to point at the target.
+
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * RotationSpeed);
+    }
+
+    void scanForTarget()
+    {
+        transform.Rotate(new Vector3(0, 0, 1), Time.deltaTime * RotationSpeed);
+    }
     void Update()
     {
-        transform.Rotate(new Vector3(0,0,1), Time.deltaTime * 100);
+        Vector2 targetPos = new Vector2(target.transform.position.x, target.transform.position.y);
+        Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+
+        float inRange = Vector2.Distance(targetPos, myPos);
+        if (inRange < 10.0f )
+        {
+            rotateToTarget();
+        }
+        else
+        {
+            scanForTarget();
+        }
     }
 }
