bf43e4f336f23655fd59d94a05b657f32649b97a
David Larsson <david.l.larsson.90@gmail.com>
2021-03-28 22:11:08 +0200

    Added random spawner

--- CuddleTreeDefenseGame/Assets/Scripts/EnemyDragon.cs
+++ CuddleTreeDefenseGame/Assets/Scripts/EnemyDragon.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 
 public class EnemyDragon : MonoBehaviour
 {
-    [SerializeField] GameObject Target;
 
     [SerializeField] public float Hp = 100;
 
+    Collider2D target;
+    bool hasTarget = false;
+
+    private void Start()
+    {
+        target = FindNearestTarget();
+    }
     // Update is called once per frame
-    void Update() 
+    void Update()
     {
         if (Hp <= 0)
         {
             Kill();
         }
-        
-        Vector3 _towerPosition = Target.transform.position;
         float step = 0.5f * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, _towerPosition, step);
+
+        if (hasTarget)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        }
+        else
+        {
+            target = FindNearestTarget();
+        }
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -33,5 +45,19 @@ public class EnemyDragon : MonoBehaviour
     {
         // Do kill animation here
         Destroy(gameObject);
+    }
+
+    private Collider2D FindNearestTarget()
+    {
+        Collider2D nearestTarget = null;
+        foreach (var target in Physics2D.OverlapCircleAll(transform.position, Mathf.Infinity))
+        {
+            if (target != null && target.gameObject.CompareTag("Tower"))
+            {
+                nearestTarget = target;
+            }
+        }
+        if (nearestTarget != null) hasTarget = true;
+        return nearestTarget;
     }
 }
