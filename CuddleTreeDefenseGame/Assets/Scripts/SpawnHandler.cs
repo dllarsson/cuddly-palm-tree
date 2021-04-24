76867b0ed59ad53420560c9b7718c81322cde3f6
Sebastian Lundgren <relinkz@users.noreply.github.com>
2021-04-24 11:12:48 +0200

    Building cant be placed on occupied slot (#25)
    * Building cant be placed on occupied slot
    
    Fixed collition with ghost building and enemies does not collide with each other
    
    * Updaterad från master
    
    Applicerat feedback

--- CuddleTreeDefenseGame/Assets/Scripts/SpawnHandler.cs
+++ CuddleTreeDefenseGame/Assets/Scripts/SpawnHandler.cs
@@ -7,6 +7,9 @@ public class SpawnHandler : MonoBehaviour
     [SerializeField] MouseButton placeMouseButton;
     private GameObject createdAsset;
     private Coroutine buildingPlacementRoutine;
+    private Color ghostCanPlace = new Color(1.0f, 1.0f, 1.0f, 0.25f);
+    private Color ghostInvalidPlacement = new Color(1.0f, 0.0f, 0.0f, 0.25f);
+    private Color ordinaryColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
     private void Start()
     {
         //Move this if we want to be able to spawn building from the start
@@ -15,19 +18,33 @@ public class SpawnHandler : MonoBehaviour
     void CreateGhostBuilding(GameObject buildingPrefab)
     {
         createdAsset = Instantiate(buildingPrefab);
-        Tools.SetColorOnGameObject(createdAsset, new Color(1, 1, 1, 0.25f));
+        Tools.SetColorOnGameObject(createdAsset, ghostCanPlace);
         Tools.ToggleScriptsInGameObject(createdAsset, false);
         buildingPlacementRoutine = StartCoroutine(FollowMouse(createdAsset));
         EventHandler.current.onMouseClick += PlaceBuilding;
     }
+    bool CanBePlacedOnSpot()
+    {
+        int nrOfCollitions = createdAsset.GetComponent<Collider2D>().OverlapCollider(new ContactFilter2D(), new Collider2D[1]);
+        if (nrOfCollitions != 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
     void PlaceBuilding(MouseButton button)
     {
         if(button == placeMouseButton)
         {
-            EventHandler.current.onMouseClick -= PlaceBuilding;
-            StopCoroutine(buildingPlacementRoutine);
-            Tools.SetColorOnGameObject(createdAsset, new Color(1, 1, 1, 1));
-            Tools.ToggleScriptsInGameObject(createdAsset, true);
+            if(CanBePlacedOnSpot())
+            {
+                EventHandler.current.onMouseClick -= PlaceBuilding;
+                StopCoroutine(buildingPlacementRoutine);
+                Tools.SetColorOnGameObject(createdAsset, ordinaryColor);
+                Tools.ToggleScriptsInGameObject(createdAsset, true);
+            }
         }
     }
     IEnumerator FollowMouse(GameObject src)
@@ -37,6 +54,15 @@ public class SpawnHandler : MonoBehaviour
             var worldpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             worldpos = Tools.GetScreenToWorldPoint2D(worldpos);
             src.transform.position = worldpos;
+            if(!CanBePlacedOnSpot())
+            {
+                Tools.SetColorOnGameObject(createdAsset, ghostInvalidPlacement);
+            }
+            else
+            {
+                Tools.SetColorOnGameObject(createdAsset, ghostCanPlace);
+            }
+
             yield return null;
         }
     }
