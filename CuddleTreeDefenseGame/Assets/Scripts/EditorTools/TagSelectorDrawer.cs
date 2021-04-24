bc054847bb5120fc91a81c4c752df8576c00e74d
Sebastian Lundgren <relinkz@users.noreply.github.com>
2021-04-24 11:56:41 +0200

    Update Proposal
    For a more cleaner approach
    
    Co-Authored-By: Zexenek <81079917+Zexenek@users.noreply.github.com>

--- CuddleTreeDefenseGame/Assets/Scripts/EditorTools/TagSelectorDrawer.cs
+++ CuddleTreeDefenseGame/Assets/Scripts/EditorTools/TagSelectorDrawer.cs
@@ -5,32 +5,29 @@ using System.Collections.Generic;
 [CustomPropertyDrawer(typeof(TagSelectorAttribute))]
 public class TagSelectorPropertyDrawer : PropertyDrawer
 {
+    public int GetElementIndexFromElementList(string find, List<string> list)
+    {
+        int index = 0;
+        for (int i = 1; i < list.Count; i++)
+        {
+            if (list[i] == find)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         if(property.propertyType == SerializedPropertyType.String)
         {
             EditorGUI.BeginProperty(position, label, property);
-
             var tagList = new List<string>(){ "<All Tags>" };
             tagList.AddRange(UnityEditorInternal.InternalEditorUtility.tags);
-            string propertyString = property.stringValue;
-            int index = -1;
-            if(propertyString == "")
-            {
-                index = 0;
-            }
-            else
-            {
-                for(int i = 1; i < tagList.Count; i++)
-                {
-                    if(tagList[i] == propertyString)
-                    {
-                        index = i;
-                        break;
-                    }
-                }
-            }
-
+            
+            int index = GetElementIndexFromElementList(property.stringValue, tagList);
             index = EditorGUI.Popup(position, label.text, index, tagList.ToArray());
             property.stringValue = index >= 1 ? tagList[index] : "";
 
